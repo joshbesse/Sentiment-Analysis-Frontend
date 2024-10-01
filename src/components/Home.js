15768740e9a5ed46706ce048bb3e1b7f32a4c29f
joshbesse <joshua.besse3@gmail.com>
2024-09-30 23:26:53 -0400

    adjusted API base URL

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ function Home() {
     const analyzeClick = async () => {
         setIsLoading(true);
         try {
-            const response = await axios.post(`${API_BASE_URL}/analyze/analyze/`, {text:text, analyzer_type:analyzerType});
+            const response = await axios.post(`${API_BASE_URL}/analyze/`, {text:text, analyzer_type:analyzerType});
             setResult(response.data)
         } catch (error) {
             setError("Error Analyzing Text")
